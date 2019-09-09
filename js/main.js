46d3102bc465795fbb28b16acd9f1eafc71c692a
feliciakarlholm <felicia.karlholm@gmail.com>
2019-09-09 03:21:04 +0200

    transitions

--- js/main.js
+++ js/main.js
@@ -1,51 +1,91 @@
-Barba.Pjax.start();
-Barba.Prefetch.init();
+import Highway from "@dogstudio/highway";
+import { TimelineLite, TweenMax } from  'gsap';
 
-var FadeTransition = Barba.BaseTransition.extend({
-   start: function() {
-      Promise
-	 .all([this.newContainerLoading, this.fadeOut()])
-	 .then(this.fadeIn.bind(this));
-   },
+var header = document.getElementsByClassName("header");
+var backGal = document.getElementsByClassName("back");
+var galleryMenu = document.getElementsByClassName("gallery-menu");
 
-   fadeOut: function() {
-      return $(this.oldContainer).animate({
-	 opacity: 0,
-	 top: '15px'
-      }).promise();
-   },
-
-   fadeIn: function() {
-      var _this = this;
-      var $el = $(this.newContainer);
-
-      $(this.oldContainer).hide();
-
-      $el.css({
-	 visibility : 'visible',
-	 opacity : 0
-      });
-
-      $el.animate({ opacity: 1 }, 400, function() {
-
-	 _this.done();
-      });
+class Default extends Highway.Transition {
+   in({from, to, done}){
+      const tl = new TimelineMax();
+      tl
+	 .fromTo( to, 0.5,
+	    { opacity: 0, y: 30 },
+	    { opacity: 1, y: 0,
+	       onComplete: function(){
+		  done();
+	       }}, 0.1
+	 )
    }
-});
+   out({from, to, done}){
+      const tl = new TimelineMax();
+      tl
+	 .fromTo( from, 0.5,
+	    { opacity: 1 },
+	    { opacity: 0, y: 30,
+	       onComplete: function(){
+		  from.remove();
+		  done();
+	       }}, 0.1
+	 )
+   }
+}
 
-var Gallery = Barba.BaseView.extend({
-   namespace: 'gallery',
-   onEnter: function() {
-      var enterGallery = new TimelineLite();
-      enterGallery.staggerFromTo( '.gallery-menu', 2,
-	 { y: 15, opacity: 0 },
-	 { y: 0, opacity: 1, ease: Expo.easeOut }, 0.3, 0.3)
-   },
-});
-
-
-Barba.Pjax.getTransition = function() {
-   Gallery.init();
-   return FadeTransition;
+class Gallery extends Highway.Transition {
+   in({from, to, done}) {
+      const tl = new TimelineMax();
+      tl
+	 .staggerFromTo( galleryMenu, 0.5,
+	    { opacity: 0, y: 15 },
+	    { opacity: 1, y: 0,
+	       onComplete: function(){
+		  done();
+	       }}, 0.1, 0.2
+	 )
+   }
+   out({from, to, done}) {
+      const tl = new TimelineMax();
+      tl
+	 .staggerFromTo( galleryMenu, 0.5,
+	    { opacity: 1, y: 0, },
+	    { opacity: 0, y: 15,
+	       onComplete: function(){
+		  from.remove();
+		  done();
+	       }}, 0.1
+	 )
+   }
 };
 
+class Gal extends Highway.Transition {
+   in({done}) {
+      const tl = new TimelineMax();
+      tl
+	 .to( header, 0.5, { opacity: 0, y: -40, display: 'none' }, 0)
+	 .fromTo( backGal, 0.5, { opacity: 0, x: -40, }, { opacity: 1, x: 0,
+	    onComplete: function(){ done(); 
+	    }}
+	 )
+   }
+   out({from, done}) {
+      const tl = new TimelineMax();
+      tl
+	 .to( backGal, 0.5, { opacity: 0, x: -40 , 
+	    onComplete: function(){
+	       from.remove();
+	       done();
+	    }}
+	 )
+	 .to( header, 0.5, { opacity: 1, y: 0, display: 'block',
+	    onComplete: function(){ done(); }}
+	 )
+   }
+}
+
+const H = new Highway.Core({
+   transitions: {
+      default: Default,
+      gallery: Gallery,
+      gal: Gal
+   }
+});
