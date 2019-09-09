03fb7721c95e41a2f172f21710bed5ddba28fa99
feliciakarlholm <felicia.karlholm@gmail.com>
2019-09-09 16:18:59 +0200

    transitions further

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 import Highway from "@dogstudio/highway";
-import { TimelineLite, TweenMax } from  'gsap';
+import { TimelineLite } from  'gsap';
 
-var header = document.getElementsByClassName("header");
-var backGal = document.getElementsByClassName("back");
+var header 	= document.getElementsByClassName("header");
+var cell 	= document.getElementsByClassName("cell");
+var backGal 	= document.getElementsByClassName("back");
 var galleryMenu = document.getElementsByClassName("gallery-menu");
 
 class Default extends Highway.Transition {
    in({from, to, done}){
       const tl = new TimelineMax();
       tl
-	 .fromTo( to, 0.5,
-	    { opacity: 0, y: 30 },
+	 .fromTo( to, 0.7,
+	    { opacity: 0, y: 10 },
 	    { opacity: 1, y: 0,
 	       onComplete: function(){
 		  done();
-	       }}, 0.1
+	       }}, 0.2
 	 )
    }
    out({from, to, done}){
@@ -22,11 +23,11 @@ class Default extends Highway.Transition {
       tl
 	 .fromTo( from, 0.5,
 	    { opacity: 1 },
-	    { opacity: 0, y: 30,
+	    { opacity: 0, y: 10,
 	       onComplete: function(){
 		  from.remove();
 		  done();
-	       }}, 0.1
+	       }}, 0
 	 )
    }
 }
@@ -35,12 +36,12 @@ class Gallery extends Highway.Transition {
    in({from, to, done}) {
       const tl = new TimelineMax();
       tl
-	 .staggerFromTo( galleryMenu, 0.5,
-	    { opacity: 0, y: 15 },
+	 .staggerFromTo( galleryMenu, 0.7,
+	    { opacity: 0, y: 9 },
 	    { opacity: 1, y: 0,
 	       onComplete: function(){
 		  done();
-	       }}, 0.1, 0.2
+	       }}, 0.1
 	 )
    }
    out({from, to, done}) {
@@ -48,7 +49,7 @@ class Gallery extends Highway.Transition {
       tl
 	 .staggerFromTo( galleryMenu, 0.5,
 	    { opacity: 1, y: 0, },
-	    { opacity: 0, y: 15,
+	    { opacity: 0, y: 9,
 	       onComplete: function(){
 		  from.remove();
 		  done();
@@ -62,22 +63,29 @@ class Gal extends Highway.Transition {
       const tl = new TimelineMax();
       tl
 	 .to( header, 0.5, { opacity: 0, y: -40, display: 'none' }, 0)
-	 .fromTo( backGal, 0.5, { opacity: 0, x: -40, }, { opacity: 1, x: 0,
-	    onComplete: function(){ done(); 
-	    }}
+	 .fromTo( backGal, 0.5, { opacity: 0, x: -40, }, { opacity: 1, x: 0
+	    }
+	 )
+	 .staggerFromTo( cell, 0.5,
+	    { opacity: 0, y: 9, },
+	    { opacity: 1, y: 0,
+	       onComplete: function(){
+		  done();
+	       }}, 0.1
 	 )
    }
    out({from, done}) {
       const tl = new TimelineMax();
       tl
-	 .to( backGal, 0.5, { opacity: 0, x: -40 , 
+	 .to( backGal, 0.5, { opacity: 0, x: -40 }
+	 )
+	 .staggerFromTo( cell, 0.5,
+	    { opacity: 1, y: 0, },
+	    { opacity: 0, y: 9}, 0.1 )
+	 .to( header, 0.5, { opacity: 1, y: 0, display: 'block',
 	    onComplete: function(){
 	       from.remove();
-	       done();
-	    }}
-	 )
-	 .to( header, 0.5, { opacity: 1, y: 0, display: 'block',
-	    onComplete: function(){ done(); }}
+	       done(); }}
 	 )
    }
 }
